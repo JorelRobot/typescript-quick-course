66e169d8ef8545db2a7d81d13580244604232560
Joel Rueda Robles <joel.rueda.robles.sc@gmail.com>
2022-12-19 14:46:44 -0600

    Tarea sobre tipados - TypeScript

---
+++ src/ejercicios/04-tarea-tipado.ts
@@ -0,0 +1,32 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+interface Direccion {
+    calle: string;
+    pais: string;
+    ciudad: string;
+}
+
+interface SuperHero {
+    nombre: string;
+    edad: number;
+    direccion: Direccion;
+    mostrarDireccion(): string; 
+};
+
+const superHero: SuperHero = {
+    nombre: 'Spiderman',
+    edad: 30,
+    direccion: {
+        calle: 'Main St',
+        pais: 'USA',
+        ciudad: 'NY'
+    },
+    mostrarDireccion() {
+        return this.nombre + ', ' + this.direccion.ciudad + ', ' + this.direccion.pais;
+    }
+};
+
+const direccion = superHero.mostrarDireccion();
+console.log(direccion);
